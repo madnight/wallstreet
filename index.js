e90b49b3e86209135e2dc944c359ad0644954916
Fabian Beuke <mail@beuke.org>
2019-08-18 19:49:27 +0200

    error handling and validation

--- index.js
+++ index.js
@@ -14,23 +14,30 @@ const { identity, values, defaultTo, pipe } = require('lodash/fp')
 const { interpolateArray }                  = require('array-interpolatejs')
 const { lookup, history }                   = require('yahoo-stocks')
 const { tail, forEach, flatMap, map, find } = require('lodash/fp')
+const { compact }                           = require('lodash/fp')
 const { toHumanString }                     = require('human-readable-numbers')
+
+const validRange = ['1m', '5m', '10m', '15m', '20m', '30m', '40m', '50m',
+'1h', '2h', '3h', '4h', '8h', '1d', '2d', '5d', '1mo', '1y', '5y', '10y']
 
 // Comand Line Parsing
 commander
   .option('-c, --chart <string>', 'chart for stock symbol e.g. MSFT')
-  .option('-i, --interval <string>', 'Interval of price changes: 1m, 1d, 5d, 1mo, 1y')
-  .option('-r, --range <string>', 'Range of dates to include: 1m, 1d, 5d, 1mo, 1y')
+  .option('-r, --range <string>', validRange.join(" "))
   .option('-h, --height <int>', 'Height of the chart')
   .option('-z, --zebra', 'Visual even-odd zebra-striped table mode')
-  .option('--width <int>', 'Width of the chart')
-  .option('-w, --watch')
+  .option('-w, --width <int>', 'Width of the chart')
   .parse(process.argv)
 
-const interval = defaultTo("1d")(commander.interval)
 const range    = defaultTo("5y")(commander.range)
 const height   = defaultTo(14)(parseInt(commander.height))
 const width    = defaultTo(80)(parseInt(commander.width))
+
+if (!validRange.includes(range)){
+    console.log(chalk.red("Invalid -r range option. See --help. " +
+    "Valid values are:\n" + validRange.join(" ")))
+    process.exit(1)
+}
 
 // Quotes API
 const iex = new IEXCloudClient(fetch,
@@ -39,15 +46,37 @@ const iex = new IEXCloudClient(fetch,
 
 // Constants
 const {COL_PAD, DELIM_LEN} = {COL_PAD: 9, DELIM_LEN: 109}
+const interval = () => {
+  if (["1mo", "1y", "5y", "10y"].includes(range))
+      return "1d"
+  if (['8h', '1d', '2d', '5d'].includes(range))
+      return "1h"
+  return "1m"
+}
 
 // Helper Functions
-const getQuote    = async i => await iex.symbols(i).batch('quote')
+const getQuote = async i => {
+  try {
+      return await iex.symbols(i).batch('quote')
+  } catch (e) {
+      console.log(chalk.red("Error. Could not find symbol: " + commander.chart))
+      process.exit(1)
+  }
+}
+const getHist = async () => {
+  try {
+      return await history(commander.chart, { interval: interval(), range: range })
+  } catch (e) {
+      console.log(chalk.red("Error. Could not find symbol: " + commander.chart))
+      process.exit(1)
+  }
+}
 const plusSign    = i => i > 0 ? '+' + i : i
 const pad         = i => i.toString().padStart(COL_PAD)
 const tablePad    = i => i.padEnd(COL_PAD)
 const dollar      = i => '$' + i.toFixed(2)
 const percentage  = i => (i * 100).toFixed(2) + '%'
-const humanString = i => i ? toHumanString(i) : null
+const humanString = i => i ? toHumanString(i).replace("G","B") : null
 const red         = pipe(pad, chalk.red)
 const green       = pipe(pad, chalk.green)
 const config      = new Configstore('stock-chart-cli',
@@ -118,16 +147,22 @@ quotes = async () => {
   )((await getQuote(config.get('stocks'))))
 }
 
+
 // Stock chart of a symbol e.g. AAPL
 const chart = async () => {
-  pipe(
-    map(identity),
-    tail,
-    flatMap(map('close')),
-    interpolateArray(width),
-    x => asciichart.plot(x, {height: height}),
-    console.log
-  )(await history(commander.chart, { interval: interval, range: range }))
+    const chart = pipe(
+      map(identity),
+      tail,
+      flatMap(map('close')),
+      interpolateArray(width),
+      compact,
+      x => asciichart.plot(x, {height: height}),
+    )(await getHist())
+    const q = map('quote')(await getQuote(commander.chart))[0]
+    console.log(chart)
+    console.log(" ".repeat(15)+(q.companyName + " " + range +
+    " chart. Latest Price: $" + q.latestPrice + ". MktCap: " +
+    humanString(q.marketCap)))
 }
 
 // Main function / Entrypoint
