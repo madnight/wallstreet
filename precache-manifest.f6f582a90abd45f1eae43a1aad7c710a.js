14d16ed77b3132a8257de4d8882195803280d2d7
Fabian Beuke <mail@beuke.org>
2020-03-18 21:21:52 +0100

    Updates

---
+++ precache-manifest.f6f582a90abd45f1eae43a1aad7c710a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c224a8ac920abb510ae6ee8fb1f85285",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caa9bb93c169f5b8a57f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5327dcad99a50016704e",
+    "url": "/static/js/2.0207d227.chunk.js"
+  },
+  {
+    "revision": "6add82288da9aa1b4791201652706bcd",
+    "url": "/static/js/2.0207d227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caa9bb93c169f5b8a57f",
+    "url": "/static/js/main.2025b03a.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
