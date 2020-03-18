1bd902519630625aba778b4b4ddf1085ceaf0f4f
Fabian Beuke <mail@beuke.org>
2020-03-18 21:28:33 +0100

    Updates

---
+++ precache-manifest.90f96ce2a51df649923c861ee19f70b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e407fc757f4cd8b008983a1418d557",
+    "url": "/wallstreet/index.html"
+  },
+  {
+    "revision": "516cf278f1a4a5a93eb6",
+    "url": "/wallstreet/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f17e42e469f627a5fe6d",
+    "url": "/wallstreet/static/js/2.1cbaadaf.chunk.js"
+  },
+  {
+    "revision": "6add82288da9aa1b4791201652706bcd",
+    "url": "/wallstreet/static/js/2.1cbaadaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "516cf278f1a4a5a93eb6",
+    "url": "/wallstreet/static/js/main.dfb6eced.chunk.js"
+  },
+  {
+    "revision": "30d71895f92e340638c4",
+    "url": "/wallstreet/static/js/runtime-main.d0358ffe.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f6f582a90abd45f1eae43a1aad7c710a.js"
+  "/wallstreet/precache-manifest.90f96ce2a51df649923c861ee19f70b5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wallstreet/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
